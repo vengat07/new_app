5dea7a994838f2f29de3b391ead5e4c438c6650f
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-09 20:14:48 +0530

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -12,13 +12,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 
+page_title = "Daibetes_Prediction and checkup"
 
+st.set_page_config(page_title = page_title)
 
-df = pd.read_csv('diabetes.csv')
+df = pd.read_csv('diabetes_1.csv')
 
 # HEADINGS
 st.title('Diabetes Checkup')
 st.sidebar.header('Patient Data')
+name = st.sidebar.text_input('Enter the name')
+st.write(df.head())
 st.subheader('Training Data Stats')
 st.write(df.describe())
 
@@ -31,16 +35,17 @@ x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random
 
 # FUNCTION
 def user_report():
-  pregnancies = st.sidebar.slider('Pregnancies', 0,17, 3 )
-  glucose = st.sidebar.slider('Glucose', 0,200, 120 )
-  bp = st.sidebar.slider('Blood Pressure', 0,122, 70 )
-  skinthickness = st.sidebar.slider('Skin Thickness', 0,100, 20 )
-  insulin = st.sidebar.slider('Insulin', 0,846, 79 )
-  bmi = st.sidebar.slider('BMI', 0,67, 20 )
-  dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.4, 0.47 )
-  age = st.sidebar.slider('Age', 21,88, 33 )
+   #name = st.sidebar.text_input('Enter the name')
+   pregnancies = st.sidebar.slider('Pregnancies', 0,17, 3 )
+   glucose = st.sidebar.slider('Glucose', 0,200, 120 )
+   bp = st.sidebar.slider('Blood Pressure', 0,122, 70 )
+   skinthickness = st.sidebar.slider('Skin Thickness', 0,100, 20 )
+   insulin = st.sidebar.slider('Insulin', 0,846, 79 )
+   bmi = st.sidebar.slider('BMI', 0,67, 20 )
+   dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.4, 0.47 )
+   age = st.sidebar.slider('Age', 21,88, 33 )
 
-  user_report_data = {
+   user_report_data = {
       'pregnancies':pregnancies,
       'glucose':glucose,
       'bp':bp,
@@ -50,8 +55,8 @@ def user_report():
       'dpf':dpf,
       'age':age
   }
-  report_data = pd.DataFrame(user_report_data, index=[0])
-  return report_data
+   report_data = pd.DataFrame(user_report_data, index=[0])
+   return report_data
 
 
 
@@ -60,7 +65,7 @@ def user_report():
 user_data_with_name = user_report()
 #user_data_with_name = user_report()
 user_data_with_name.insert(0,"name",name,True)
-#df.insert(2, "Age", [21, 23, 24, 21], True)
+
 st.subheader('Patient Data')
 st.write(user_data_with_name)
 user_data_witout_name = user_data_with_name.drop('name',axis=1)
