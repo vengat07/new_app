0efe601fac13fd5168f3f08ce6bb5c0467a4c769
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-09 22:13:41 +0530

    Update value_edit.py

--- value_edit.py
+++ value_edit.py
@@ -29,13 +29,13 @@ st.write(df.describe())
 x = df.drop(['Outcome'], axis = 1)
 y = df.iloc[:, -1]
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
-
+ 
 
 # FUNCTION
 import streamlit as st
 import pandas as pd
 
-def user_report():
+def user_report(df):
     name = st.sidebar.text_input('Name')
     pregnancies = st.sidebar.slider('Pregnancies', 0,17, )
     glucose = st.sidebar.slider('Glucose', 0,200, )
@@ -46,10 +46,9 @@ def user_report():
     dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.4,  )
     age = st.sidebar.slider('Age', 0,100, )
 
-    
     submit_button = st.sidebar.button('Submit')
     if not submit_button:
-        return None
+        return df
     
     user_report_data = {
         'name': name,
@@ -62,8 +61,11 @@ def user_report():
         'dpf': dpf,
         'age': age
     }
-    report_data = pd.DataFrame(user_report_data, index=[0])
-    return report_data
+    
+    user_data = pd.DataFrame(user_report_data, index=[0])
+    new_df = new_df.append(user_data, ignore_index=True)
+    
+    return new_df
 
 
 # PATIENT DATA
@@ -83,19 +85,7 @@ rf.fit(x_train, y_train)
 user_result = rf.predict(user_data)
 
 
-# Age vs Dpf
-#st.header('DPF Value Graph (Others vs Yours)')
-#fig_dpf = plt.figure()
-#ax13 = sns.scatterplot(x = 'Age', y = 'DiabetesPedigreeFunction', data = df, hue = 'Outcome', palette='YlOrBr')
-#ax14 = sns.scatterplot(x = user_data['age'], y = user_data['dpf'], s = 150, color = color)
-#plt.xticks(np.arange(10,100,5))
-#plt.yticks(np.arange(0,3,0.2))
-#plt.title('0 - Healthy & 1 - Unhealthy')
-#st.pyplot(fig_dpf)
 
-#ploting
-final=pd.Series(rfc.feature_importances_, index=X.columns).plot(kind='barh')
-st.plot(final)
 
 # OUTPUT
 st.subheader('Your Report: ')
