14e6a5462f6e0a5ddf77c5cb7d41f146457e6268
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-09 21:36:09 +0530

    Update value_edit.py

--- value_edit.py
+++ value_edit.py
@@ -41,10 +41,10 @@ def user_report():
     glucose = st.sidebar.slider('Glucose', 0,200, )
     bp = st.sidebar.slider('Blood Pressure', 0,122, )
     skinthickness = st.sidebar.slider('Skin Thickness', 0,100,)
-    insulin = st.sidebar.slider('Insulin', 0,846,)
+    insulin = st.sidebar.slider('Insulin', 0,300,)
     bmi = st.sidebar.slider('BMI', 0,67, )
     dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.4,  )
-    age = st.sidebar.slider('Age', 21,88, )
+    age = st.sidebar.slider('Age', 0,100, )
 
     
     submit_button = st.sidebar.button('Submit')
