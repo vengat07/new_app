e1984944747b247e313322504014766e8c2bf1b1
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-11 14:53:30 +0530

    Update value_edit.py

--- value_edit.py
+++ value_edit.py
@@ -58,7 +58,7 @@ def user_report():
       }
       report_data = pd.DataFrame(user_report_data, index=[0])
       if glucose > 125 and insulin < 70 and bmi < 25 and age > 40:
-         outresult = 'You are Type I Diabetes
+         outresult = 'You are Type I Diabetes'
         '
         
       else:
