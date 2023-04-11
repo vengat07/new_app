2232a1d0044efb8d22dd0969dfaf385bdf550d19
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-11 15:01:49 +0530

    Update value_edit.py

--- value_edit.py
+++ value_edit.py
@@ -80,6 +80,75 @@ rf.fit(x_train, y_train)
 user_result = rf.predict(user_data)
 
 
+# VISUALISATIONS
+st.title('Visualised Patient Report')
+
+# COLOR FUNCTION
+if user_result[0]==0:
+  color = 'blue'
+else:
+  color = 'red'
+
+
+# Age vs Pregnancies
+st.header('Pregnancy count Graph (Others vs Yours)')
+fig_preg = plt.figure()
+ax1 = sns.scatterplot(x = 'Age', y = 'Pregnancies', data = df, hue = 'Outcome', palette = 'Greens')
+ax2 = sns.scatterplot(x = user_data['age'], y = user_data['pregnancies'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,20,2))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_preg)
+
+
+
+# Age vs Glucose
+st.header('Glucose Value Graph (Others vs Yours)')
+fig_glucose = plt.figure()
+ax3 = sns.scatterplot(x = 'Age', y = 'Glucose', data = df, hue = 'Outcome' , palette='magma')
+ax4 = sns.scatterplot(x = user_data['age'], y = user_data['glucose'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,220,10))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_glucose)
+
+
+
+# Age vs Bp
+st.header('Blood Pressure Value Graph (Others vs Yours)')
+fig_bp = plt.figure()
+ax5 = sns.scatterplot(x = 'Age', y = 'BloodPressure', data = df, hue = 'Outcome', palette='Reds')
+ax6 = sns.scatterplot(x = user_data['age'], y = user_data['bp'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,130,10))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_bp)
+
+
+
+# Age vs Insulin
+st.header('Insulin Value Graph (Others vs Yours)')
+fig_i = plt.figure()
+ax9 = sns.scatterplot(x = 'Age', y = 'Insulin', data = df, hue = 'Outcome', palette='rocket')
+ax10 = sns.scatterplot(x = user_data['age'], y = user_data['insulin'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,900,50))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_i)
+
+
+# Age vs BMI
+st.header('BMI Value Graph (Others vs Yours)')
+fig_bmi = plt.figure()
+ax11 = sns.scatterplot(x = 'Age', y = 'BMI', data = df, hue = 'Outcome', palette='rainbow')
+ax12 = sns.scatterplot(x = user_data['age'], y = user_data['bmi'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,70,5))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_bmi)
+
+
+
 # OUTPUT
 st.subheader('Your Report: ')
 output=''
